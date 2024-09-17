05b5d0a27cf8554106929ee4d746e8ecabf41b71
eteen <eteensmash@gmail.com>
2024-09-16 19:44:15 -0700

    styling for nav bar complete

--- hooks/useScrollEffect.js
+++ hooks/useScrollEffect.js
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react";
 export function useScrollEffect() {
   const [scrollingDown, setScrollingDown] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0); //loads 0 when is at the top of the page
+  const [currentScrollPos, setCurrentScrollPos] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY; //this gets the current scroll position from the top of the page
+      setCurrentScrollPos(currentScrollPos);
       setScrollingDown(currentScrollPos > prevScrollPos); // it will set scrollingDown useState to (true), if current scroll is greater than previous, meaning that the nav bar will disapear
       setPrevScrollPos(currentScrollPos); // sets the prevScrollPos to the current one, so that the above function can work
     };
@@ -14,5 +16,5 @@ export function useScrollEffect() {
     return () => window.removeEventListener("scroll", handleScroll); //stops listening to avoid memory leaks
   }, [prevScrollPos]); //by using a dependancy array the effect only re-runs when one of the dependencies change, without it it might use outdated values of prevScrollPos
 
-  return scrollingDown;
+  return { scrollingDown,currentScrollPos };
 }
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -14,52 +14,44 @@ import { useScrollEffect } from "/hooks/useScrollEffect";
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const scrollingDown = useScrollEffect();
+  const { scrollingDown, currentScrollPos } = useScrollEffect();
 
   const handleNav = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const background = currentScrollPos > 0 ? "black" : "transparent";
+  const shadow= currentScrollPos > 0 ? "0 4px 6px rgba(0, 0, 0, 0.5)" : "";
+
   return (
     <nav
-      className={`fixed top-0 left-0 w-full h-28 lg:h-32 transition-transform duration-300 ${
-        scrollingDown ? "-translate-y-full" : "translate-y-0"
-      }`}
+      className={`fixed top-0 left-0 w-full h-24 lg:h-28 transition-transform duration-700 ${scrollingDown ? "-translate-y-full" : "translate-y-0"}`}
+      style={{
+        backgroundColor: background,
+        transition: "transform 0.5s ease-out, background-color 0.5s ease-out",
+        boxShadow: shadow,
+      }}
     >
       <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
         <Link href="/">
           <div className="relative w-40 h-40 sm:w-48 sm:h-40 md:w-56 md:h-48 lg:w-72 lg:h-64">
-            <Image
-              src={Logo}
-              alt="logo"
-              layout="fill"
-              objectFit="contain"
-              priority
-            />
+            <Image src={Logo} alt="logo" layout="fill" objectFit="contain" priority />
           </div>
         </Link>
         <div className="hidden sm:flex items-center">
           <ul className="flex items-center">
             <Link href="/services">
-              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">
-                Services
-              </li>
+              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">Services</li>
             </Link>
             <Link href="/contact">
-              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">
-                Contact
-              </li>
+              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">Contact</li>
             </Link>
             <Link href="/about">
-              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">
-                About
-              </li>
+              <li className="ml-10 hover:border-b text-xl text-white lg:text-2xl">About</li>
             </Link>
             <li className="ml-10">
               <a href="tel:+1234567890">
-                <button className="bg-red-700 text-white lg:text-xl py-2 px-4 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110">
-                  Call Us
-                </button>
+                <button className="bg-red-700 text-white lg:text-xl py-2 px-4 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110">Call Us</button>
               </a>
             </li>
           </ul>
@@ -69,58 +61,45 @@ export default function NavBar() {
         </div>
       </div>
       <div
-        className={`fixed top-0 right-0 w-[100%] sm:hidden h-screen bg-[#080808] p-10 transition-opacity ease-in duration-500 ${
-          menuOpen ? "opacity-100 visible z-50" : "opacity-0 invisible -z-50"
-        }`}
+        className={`fixed top-0 right-0 w-[100%] sm:hidden h-screen bg-[#080808] p-10 transition-opacity ease-in duration-500 ${menuOpen ? "opacity-100 visible z-50" : "opacity-0 invisible -z-50"}`}
         style={{
           position: "fixed",
           transition: "opacity 0.5s ease-out, visibility 0.5s ease-out",
         }}
       >
-        <div className="flex w-full items-center justify-end ml-7 -mt-2 transition-opacity ">
+
+
+
+        <div className="absolute top-0 left-4 -mt-6 w-40 h-40 sm:w-48 sm:h-40 md:w-56 md:h-48 lg:w-72 lg:h-64">
+          <Image src={Logo} alt="logo" layout="fill" objectFit="contain" priority />
+        </div>
+        <div className="flex w-full items-center justify-end ml-7 -mt-2 transition-opacity">
           <div onClick={handleNav} className="cursor-pointer">
             <AiOutlineClose size={30} className="text-white" />
           </div>
         </div>
-        <div className="flex-col  py-4">
-          <ul className="w-full text-center">
+        <div className="flex-col py-4 mt-20">
+          <ul className="w-full">
             <Link href="/services">
-              <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer text-white text-2xl"
-              >
-                Services
-              </li>
+              <li onClick={() => setMenuOpen(false)} className="py-5 cursor-pointer text-white text-xl border-b border-white">Services</li>
             </Link>
-            <Link href="about">
-              <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer text-white text-2xl"
-              >
-                About
-              </li>
+            <Link href="/about">
+              <li onClick={() => setMenuOpen(false)} className="py-5 cursor-pointer text-white text-xl border-b border-white">About</li>
             </Link>
-            <Link href="contact">
-              <li
-                onClick={() => setMenuOpen(false)}
-                className="py-4 cursor-pointer text-white text-2xl"
-              >
-                Contact
-              </li>
+            <Link href="/contact">
+              <li onClick={() => setMenuOpen(false)} className="py-5 cursor-pointer text-white text-xl border-b border-white">Contact</li>
             </Link>
           </ul>
         </div>
-        <div className="flex items-center space-x-5 justify-center mt-">
-          <AiOutlineInstagram size={50} className="cursor-pointer text-white" />
-          <AiOutlineFacebook size={50} className="cursor-pointer text-white" />
-          <AiOutlineTwitter size={50} className="cursor-pointer text-white" />
-        </div>
-        <div className="bottom-0 left-0 right-0 flex justify-center items-center p-4">
+        <div className="bottom-0 left-0 right-0 flex justify-center items-center p-4 text-nowrap">
           <a href="tel:+1234567890">
-            <button className="bg-red-700 text-white text-2xl py-2 px-36 rounded-full transition-colors duration-150 ease-in-out active:bg-blue-300">
-              Call us
-            </button>
+            <button className="bg-red-700 text-white text-2xl py-2 px-28 w-full rounded-full transition-colors duration-150 ease-in-out active:bg-blue-300">Call us</button>
           </a>
+        </div>
+        <div className="flex items-center space-x-4 justify-center mt-4">
+          <AiOutlineInstagram size={40} className="cursor-pointer text-white" />
+          <AiOutlineFacebook size={40} className="cursor-pointer text-white" />
+          <AiOutlineTwitter size={40} className="cursor-pointer text-white" />
         </div>
       </div>
     </nav>
